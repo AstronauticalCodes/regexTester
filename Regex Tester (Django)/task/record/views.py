ec8e2a8ac9566079e9f28489727833d70c802ea7
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-14 17:12:07 +0530

    STAGE-3-Added post method inside MainView.

--- Regex Tester (Django)/task/record/views.py
+++ Regex Tester (Django)/task/record/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import MainForm
+from django.http import HttpResponse
+import re
 # Create your views here.
 
 
@@ -10,3 +12,10 @@ class MainView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, context={'form': self.form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if form.is_valid():
+            match = re.match(request.POST.regex, request.POST.text)
+            form.save()
+            return HttpResponse(f"<h2>{True if match else False}</h2>")
