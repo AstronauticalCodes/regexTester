b993fb0675db8b503ccc1dc9cdccbd460399b6f2
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-15 13:01:36 +0530

    STAGE-4-Added template and routing for history page.

--- Regex Tester (Django)/task/record/urls.py
+++ Regex Tester (Django)/task/record/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.MainView.as_view()),
     path('result/<int:pk>/', views.ResultView.as_view(), name='result'),
+    path('history/', views.HistoryView.as_view(), name='history'),
 ]
